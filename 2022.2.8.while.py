fa4530a4355f19b1c8a5e3136e18bfb64b8c5272
yolo <77111763+yoloeww@users.noreply.github.com>
2022-02-28 22:37:54 +0800

    Create 2022.2.8.while.py

---
+++ 2022.2.8.while.py
@@ -0,0 +1,27 @@
+i = 0
+n = 100
+sum = 0
+while i <= n:
+      sum = sum+i
+      i = i+1
+print("1-%d=%d"%(n,sum))
+j=0
+sum=0
+for j in range(0, 101, 1):
+    sum=sum+j
+print("sum=%d"%sum)
+count = 0
+while count <5:
+    print(count,"小于5")
+    count+=1
+else :
+    print(count,"大于或者等于5")
+'''
+
+i =0
+while i<10:
+    i=i+1
+    print("-"*10)
+    if i==5:
+        continue
+    print(i)
