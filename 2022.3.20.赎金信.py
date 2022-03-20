1fabe2d72ecee842ec81f7f151df6fa1e829fe40
yolo <77111763+yoloeww@users.noreply.github.com>
2022-03-20 23:37:25 +0800

    Create 2022.3.20.赎金信.py

---
+++ 2022.3.20.赎金信.py
@@ -0,0 +1,5 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+        return not collections.Counter(ransomNote) - collections.Counter(magazine)
